5e66a6010c04ff64b23961592e03dc434cf45b87
Rene Morais <rene.douglas.bsi@gmail.com>
2024-10-18 21:44:15 -0300

    ajuste package

--- src/main/java/br/edu/ifpi/supermarket/configuration/LiveReloadConfig.java
+++ src/main/java/br/edu/ifpi/supermarket/configuration/LiveReloadConfig.java
@@ -1,4 +1,4 @@
-package br.edu.ifpi.supermarket.Configuration;
+package br.edu.ifpi.supermarket.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
